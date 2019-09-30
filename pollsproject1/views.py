16df8c21187c56dd746ee0f66be86f834ad9de40
Rahab <rahabnderitu88@gmail.com>
2019-09-30 16:51:05 +0300

    Django tasks

--- pollsproject1/views.py
+++ pollsproject1/views.py
@@ -1,25 +1,38 @@
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.template import loader
-from django.http import HttpResponse,HttpResponseRedirect
-
+from django.http import HttpResponseRedirect
+from django.views import generic
+from django.utils import timezone
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('pollsproject1/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pollsproject1/detail.html', {'question': question})    
-      
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pollsproject1/results.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'pollsproject1/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'pollsproject1/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'pollsproject1/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -40,12 +53,9 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('pollsproject1:results', args=(question.id,)))
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pollsproject1/results.html', {'question': question})    
+    
 
-
-
+    
 
 
 
